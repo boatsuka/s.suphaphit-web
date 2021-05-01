2cc7c55dde70031bb79a73d94a734f745216d99d
BoatDev101 <76729205+BoatDev101@users.noreply.github.com>
2021-05-01 20:19:46 +0700

    Update function && comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,47 +1,57 @@
 (function () {
-
 	`use strict`
 
-  const fetchCovidTH = (url) => {
-    fetch(url)
-      .then((response) => {
-        if (response.status !== 200) {
-          console.log('Look like there was a problem. Status Code: ' + response.status)
-          return response.json()
-        }
-        response.json().then(function (data) {
-          document.getElementById('confirmed-th').innerHTML =
-            new Intl.NumberFormat().format(data.confirmed.value) + ' คน'
-          document.getElementById('recovered-th').innerHTML =
-            new Intl.NumberFormat().format(data.recovered.value) + ' คน'
-          document.getElementById('deaths-th').innerHTML =
-            new Intl.NumberFormat().format(data.deaths.value) + ' คน'
-          document.getElementById('date-th').innerHTML = 'อัพเดทเมื่อวันที่ ' + data.lastUpdate
-        })
-      })
-      .catch((error) => {
-        throw new error()
-      })
-  }
+	//! Settings variable function fetchCovidTH
+	var confirmedThai = document.querySelector(`#confirmed-th`),
+		recoveredThai = document.querySelector(`#recovered-th`),
+		deathsThai = document.querySelector(`#deaths-th`),
+		dateThai = document.querySelector(`#date-th`)
 
-  const fetchCovidGlobal = (url) => {
-    fetch(url).then((response) => {
-      if (response.status !== 200) {
-        console.log('Look like there was a problem. Status Code: ' + response.status)
-        return response.json()
-      }
+	//! Settings variable function fetchCovidGlobal
+	var confirmedGlobal = document.querySelector(`#confirmed-global`),
+		recoveredGlobal = document.querySelector(`#recovered-global`),
+		deathsGlobal = document.querySelector(`#deaths-global`)
 
-      response.json().then(function (data) {
-        document.getElementById('confirmed-global').innerHTML =
-          new Intl.NumberFormat().format(data.confirmed.value) + ' คน'
-        document.getElementById('recovered-global').innerHTML =
-          new Intl.NumberFormat().format(data.recovered.value) + ' คน'
-        document.getElementById('deaths-global').innerHTML =
-          new Intl.NumberFormat().format(data.deaths.value) + ' คน'
-      })
+	//? function fetchCovidTH
+	function fetchCovidTH (url) {
+		fetch(url)
+			.then((response) => {
+				if (response.status !== 200) {
+					return response.json()
+				}
+				response.json().then(function (data) {
+					confirmedThai.innerHTML =
+						new Intl.NumberFormat().format(data.confirmed.value) + " คน"
+					recoveredThai.innerHTML =
+						new Intl.NumberFormat().format(data.recovered.value) + " คน"
+					deathsThai.innerHTML = new Intl.NumberFormat().format(data.deaths.value) + " คน"
+					dateThai.innerHTML = "อัพเดทเมื่อวันที่ " + data.lastUpdate
+				})
+			})
+			.catch((error) => {
+				throw new error()
+			})
+	}
+
+	//? function fetchCovidGlobal
+	function fetchCovidGlobal(url) {
+		fetch(url).then((response) => {
+			if (response.status !== 200) {
+				return response.json()
+			}
+			response.json().then(function (data) {
+				confirmedGlobal.innerHTML =
+					new Intl.NumberFormat().format(data.confirmed.value) + " คน"
+				recoveredGlobal.innerHTML =
+					new Intl.NumberFormat().format(data.recovered.value) + " คน"
+				deathsGlobal.innerHTML = new Intl.NumberFormat().format(data.deaths.value) + " คน"
+			})
+		})
+    .catch((error) => {
+      throw new error
     })
-  }
+	}
 
-  fetchCovidTH('https://covid19.mathdro.id/api/countries/TH')
-  fetchCovidGlobal('https://covid19.mathdro.id/api')
+	fetchCovidTH("https://covid19.mathdro.id/api/countries/TH")
+	fetchCovidGlobal("https://covid19.mathdro.id/api")
 })()
